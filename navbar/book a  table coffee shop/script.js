6b65223b4302e640d4f2014aa04bbea55533fb86
Manish yadav <manishyadav9260988060@gmail.com>
2024-09-25 18:06:38 +0530

    Delete navbar directory

--- navbar/book a  table coffee shop/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#bookingForm').bookingForm({
-      ownerEmail: 'your-email@example.com'
-    });
-  });
-  
-  $(function() {
-    $('#bookingForm input, #bookingForm textarea').placeholder();
-  });
